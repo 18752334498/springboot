4208f27cc0abef37c2ebfe1a09a42b3976ae1606
2424693824ls <2424693824@qq.com>
2019-06-01 14:51:29 +0800

    fsdfsfds

--- src/main/java/com/yucong/aspect/CacheRedisAspect.java
+++ src/main/java/com/yucong/aspect/CacheRedisAspect.java
@@ -15,33 +15,33 @@ import Utils.SpelParser;
 @Component
 public class CacheRedisAspect {
 
-	@Autowired
-	private RedisTemplate<Object, Object> redisTemplate;
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
 
-	@Around("@annotation(crs)")
-	public Object doAround(ProceedingJoinPoint pjp, CacheRedisByOneParam crs) {
-		String value = (String) SpelParser.getKey(crs.value(), pjp);
-		String prefix = crs.prefix();
-		System.out.println(pjp.getSourceLocation().getWithinType().getName());// 类名
-		System.out.println(pjp.getSignature().getName());// 方法名
-		System.out.println(pjp.getArgs());// 参数值
+    @Around("@annotation(crs)")
+    public Object doAround(ProceedingJoinPoint pjp, CacheRedisByOneParam crs) {
+        String value = (String) SpelParser.getKey(crs.value(), pjp);
+        String prefix = crs.prefix();
+        System.out.println(pjp.getSourceLocation().getWithinType().getName());// 类名
+        System.out.println(pjp.getSignature().getName());// 方法名
+        System.out.println(pjp.getArgs());// 参数值
 
-		Object result = redisTemplate.opsForValue().get(prefix + "_" + value);
-		if (result != null) {
-			System.out.println("缓存取值。。。");
-			return result;
-		}
-		System.out.println("缓存没值！！！");
-		try {
-			result = pjp.proceed();
-			if (result != null) {
-				redisTemplate.opsForValue().set(prefix + "_" + value, result);
-				System.out.println("将值加入缓存。。。");
-			}
-		} catch (Throwable e) {
-			System.out.println("程序异常。。。");
-			e.printStackTrace();
-		}
-		return result;
-	}
+        Object result = redisTemplate.opsForValue().get(prefix + "_" + value);
+        if (result != null) {
+            System.out.println("缓存取值。。。");
+            return result;
+        }
+        System.out.println("缓存没值！！！");
+        try {
+            result = pjp.proceed();
+            if (result != null) {
+                redisTemplate.opsForValue().set(prefix + "_" + value, result);
+                System.out.println("将值加入缓存。。。");
+            }
+        } catch (Throwable e) {
+            System.out.println("程序异常。。。");
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
